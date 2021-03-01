9c36d80d9cfef94f4732e8aa8c525b2f7c5c9c30
Ankit Pithalia <apithalia@teksystems.com>
2021-03-01 14:31:28 +0530

    Student Record stored in data.txt Displaying complete code

--- ProjectBasicsofCsharp/Fileprogram.cs
+++ ProjectBasicsofCsharp/Fileprogram.cs
@@ -9,38 +9,17 @@ namespace ProjectBasicsofCsharp
     {
         public static void Do()
         {
-            Writetext();
+            
             readText();
         }
-        private static void Writetext()
-        {
-            string dir = Directory.GetCurrentDirectory();
-            string filename = dir + "\\data.txt";
-            if (File.Exists(filename))
-            {
-                Console.WriteLine("File exists");
-            }
-            else
-            {
-                Console.WriteLine("File doesn't exist");
-            }
-
-            StreamWriter wrt = File.CreateText(filename);
-            wrt.WriteLine("Name: Ankit, Age: 21, Dept: Traniee");
-            wrt.WriteLine("Name: Shaswat, Age: 21, Dept: Devops");
-            wrt.WriteLine("Name: Yash, Age: 21, Dept: Traniee");
-            wrt.WriteLine("Name: John, Age: 21, Dept: CD");
-            wrt.WriteLine("Name: Adam, Age: 21, Dept: IT");
-            wrt.Close();
-            Console.WriteLine("Content lines have been added into file");
-        }
+        
         private static void readText()
         {
             string dir = Directory.GetCurrentDirectory();
             string filename = dir + "\\data.txt";
             if (File.Exists(filename))
             {
-                Console.WriteLine("File exists");
+                Console.WriteLine("Students Record Present in the Database are:");
                 string[] contents = File.ReadAllLines(filename);
                 foreach (var content in contents)
                 {
