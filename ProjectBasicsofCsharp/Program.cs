f73d6e57182380fe931d3530ac9c59657ef4dea9
Ankit Pithalia <apithalia@teksystems.com>
2021-03-01 12:52:17 +0530

    Add project files.

---
+++ ProjectBasicsofCsharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectBasicsofCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project started");
+            Fileprogram.Do();
+        }
+    }
+}
