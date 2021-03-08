597a78df59063ce5b00955813f3d98482eb23ea6
Ankit Pithalia <apithalia@teksystems.com>
2021-03-08 14:07:13 +0530

    It will contain the name and class section of the teacher.

---
+++ OopsProject/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopsProject
+{
+    class Teacher
+    {
+        public string Name { get; set; }
+        public string ClassAndSection { get; set; }
+
+
+
+    }
+}
