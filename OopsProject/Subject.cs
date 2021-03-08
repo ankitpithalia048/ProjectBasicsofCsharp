7b31d5de9e8e60e48d257ae7aa84a31abdcc6951
Ankit Pithalia <apithalia@teksystems.com>
2021-03-08 14:06:49 +0530

    Subject class contain subject name and code. it will inherit two classes Print and teacher to display the subject taught by teacher.

---
+++ OopsProject/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopsProject
+{
+    class Subject : Teacher, Print
+    {
+        public string SubjectName { get; set; }
+        public string SubjectCode { get; set; }
+        public string GetInfo()
+        {
+            return "Subject Name: "+SubjectName+ "; Teacher Name: " + Name + "; Class&Section: " + ClassAndSection+"; Subject code: "+SubjectCode;
+        }
+    }
+}
