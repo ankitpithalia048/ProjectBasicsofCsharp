f538294e0b02c4dc0c3cc66d164fb36f4e869d2c
Ankit Pithalia <apithalia@teksystems.com>
2021-03-08 14:03:04 +0530

    Student information are stored and it will inherit the Print class to display the student in the class.

---
+++ OopsProject/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopsProject
+{
+    class Student : Print
+    {
+        public string Name { get; set; }
+        public string ClassAndSection { get; set; }
+
+
+        public virtual string GetInfo()
+        {
+            return "Name: " + Name + ", Class&Section: " + ClassAndSection;
+        }
+
+    }
+}
